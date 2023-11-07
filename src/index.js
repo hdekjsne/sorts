4b8c158da5a17429d435f7c11305946f0fb5617c
Анастасия Мамаева <hdekjsne@gmail.com>
2023-11-07 18:47:09 +0300

    new bubble sort

--- src/index.js
+++ src/index.js
@@ -3,20 +3,25 @@ export default class AllSorts {
 	// 1. Bubble Sort
 	bubble(arr) {
 		let changesCount = 0;
-		for(let i = 0; i < arr.length; i += 1) {
+		let i = 0;
+		let lastElIndex = arr.length - 1;
+		while(i <= lastElIndex) {
 			if (arr[i] > arr[i + 1]) {
 				[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
 				changesCount += 1;
 			}
-			if (i === arr.length - 1) {
-				i = (changesCount > 0 ? -1 : i + 2);
+			if (i === lastElIndex) {
+				i = (changesCount > 0 ? -1 : i);
 				changesCount = 0;
+				lastElIndex -= 1;
 			}
+			i += 1;
 		}
 		return arr;
 	}
 
 	// 2. Optimized Bubble Sort
+
 	// 3. Coctail Shaker Sort
 	// 4. Optimized Coctail Shaker Sort
 }

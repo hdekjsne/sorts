2a2d248eaeda14cd6b40024c16d32fd4f4110c82
Анастасия Мамаева <hdekjsne@gmail.com>
2023-11-07 12:07:05 +0300

    bubble sort + tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,18 @@
+import AllSorts from "../src/index.js";
+
+const sorts = new AllSorts();
+let ex1;
+let ex2;
+const sorted1 = [0, 1, 5, 5, 6, 10, 20, 22, 25, 44, 80, 91, 100];
+const sorted2 = [0, 1, 1, 3, 4, 4, 5, 10, 38, 44, 90];
+
+beforeEach(() => {
+	ex1 = [1, 0, 10, 100, 20, 22, 25, 6, 5, 44, 91, 80, 5];
+	ex2 = [1, 4, 4, 5, 3, 10, 0, 90, 1, 44, 38];
+})
+
+test('bubble sort', () => {
+	expect(sorts.bubble([])).toStrictEqual([]);
+	expect(sorts.bubble(ex1)).toStrictEqual(sorted1);
+	expect(sorts.bubble(ex2)).toStrictEqual(sorted2);
+})
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-export default lala = () => 'lala';
+export default class AllSorts {
+	
+	// 1. Bubble Sort
+	bubble(arr) {
+		let changesCount = 0;
+		for(let i = 0; i < arr.length; i += 1) {
+			console.log(i);
+			if (arr[i] > arr[i + 1]) {
+				[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+				changesCount += 1;
+			}
+			if (i === arr.length - 1) {
+				i = (changesCount > 0 ? 0 : i + 2);
+				changesCount = 0;
+			}
+		}
+		return arr;
+	}
+
+	// 2. Optimized Bubble Sort
+	// 3. Coctail Shaker Sort
+	// 4. Optimized Coctail Shaker Sort
+}
